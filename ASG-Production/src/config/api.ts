791dd4b2850f917ae9637337e7a1df820ae0d153
boyagus233 <rahmatserang526@gmail.com>
2026-08-10 23:40:02 +0700

    config: update API_BASE_URL to MikroTik Public IP (http://31.58.59.223:3000)

--- ASG-Production/src/config/api.ts
+++ ASG-Production/src/config/api.ts
@@ -1,2 +1,2 @@
-// URL HTTPS Publik Backend Cloudflare Tunnel
-export const API_BASE_URL = 'https://cheats-plant-picture-rom.trycloudflare.com';
+// URL IP Public Backend Server MikroTik Permanen
+export const API_BASE_URL = 'http://31.58.59.223:3000';
